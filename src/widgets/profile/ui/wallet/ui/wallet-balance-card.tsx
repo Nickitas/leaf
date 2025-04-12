46e2c5a5b7a6adea197f23d81797930d73ee8395
NeDatsky <nickitadatsky@gmail.com>
2025-04-13 02:09:43 +0300

    add modal refill

--- src/widgets/profile/ui/wallet/ui/wallet-balance-card.tsx
+++ src/widgets/profile/ui/wallet/ui/wallet-balance-card.tsx
@@ -1,32 +1,41 @@
+"use client";
+
 import React, { FC } from "react";
 import { walletList } from "../model/mock/wallets-list";
 import { Card, CardBody, CardHeader } from "@heroui/card";
 import { Button } from "@heroui/button";
 import { EcoProgress } from "@/shared/ui/progress/EcoProgress";
+import { WalletRefillModal } from "./wallet-refill-modal";
+import { useWalletTransactions } from "../model/store/wallet-refill-modal-store";
 
 export const WalletBalanceCard: FC = () => {
 
     const progress = (walletList.balance / walletList.goal) * 100;
 
+    const { isOpen, onOpen } = useWalletTransactions();
+
     return (
-        <Card className="bg-gradient-to-r from-primary-50 to-primary-100 dark:from-primary-900 dark:to-primary-800">
-            <CardHeader>
-                <h2 className="text-xl font-semibold">Мой баланс</h2>
-            </CardHeader>
-            <CardBody>
-                <div className="flex items-end justify-between">
-                    <div>
-                        <p className="text-4xl font-bold">{walletList.balance} ₽</p>
-                        <p className="text-sm text-gray-600 dark:text-gray-400 mt-2">
-                            Цель: {walletList.goal} ₽
-                        </p>
+        <>
+            <Card className="bg-gradient-to-r from-primary-50 to-primary-100 dark:from-primary-900 dark:to-primary-800">
+                <CardHeader>
+                    <h2 className="text-xl font-semibold">Мой баланс</h2>
+                </CardHeader>
+                <CardBody>
+                    <div className="flex items-end justify-between">
+                        <div>
+                            <p className="text-4xl font-bold">{walletList.balance} ₽</p>
+                            <p className="text-sm text-gray-600 dark:text-gray-400 mt-2">
+                                Цель: {walletList.goal} ₽
+                            </p>
+                        </div>
+                        <Button color="primary" variant="solid" size="lg" onPress={onOpen}>
+                            Пополнить
+                        </Button>
                     </div>
-                    <Button variant="solid" size="lg">
-                        Пополнить
-                    </Button>
-                </div>
-                <EcoProgress value={progress} className="mt-4" />
-            </CardBody>
-        </Card>
+                    <EcoProgress value={progress} className="mt-4" />
+                </CardBody>
+            </Card>
+            { isOpen && <WalletRefillModal />}
+        </>
     );
 };
