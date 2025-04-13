a1a6fdd50c16f37e4f657fc8e24f893768d6defa
NeDatsky <nickitadatsky@gmail.com>
2025-04-13 12:41:36 +0300

    test

--- src/app/profile/impact/page.tsx
+++ src/app/profile/impact/page.tsx
@@ -1,14 +1,8 @@
 import React from "react";
 import { Impact } from "@/views/profile/Impact";
 
-const ecoImpact = {
-  treesSaved: 12,
-  co2Reduced: 150,
-  plasticReduced: 8
-}
-
 export default function ImpactPage() {
   return (
-    <Impact level={3} progress={10} ecoImpact={ecoImpact} />
+    <Impact />
   );
 }
